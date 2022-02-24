96c3b4d35a224e5edeb9141b684e80e88c887dd7
Artem IG <ortemeo@gmail.com>
2022-02-24 03:59:49 +0300

    upd

--- test_pkg.py
+++ test_pkg.py
@@ -2,6 +2,5 @@ from chkpkg import Package
 
 if __name__ == "__main__":
     with Package() as pkg:
-        pkg.run_python_code('from tempp import TempGradleApp, '
-                            'RunResult, UnexpectedOutput')
+        pkg.run_python_code('from tempp import TempProject')
     print("\nPackage is OK!")
